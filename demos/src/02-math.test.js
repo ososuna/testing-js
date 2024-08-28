f4d091375e9fa364f859b334e221e8e277aa8ccb
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-08-27 19:41:13 -0600

    math test

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,19 @@
+const {sum, multiply, divide} = require('./02-math');
+
+test('adds 1 + 3 to equal 4', () => {
+  expect(sum(1, 3)).toBe(4);
+});
+
+test('multiply 1 * 4 to equal 4', () => {
+  expect(multiply(1, 4)).toBe(4);
+});
+
+test('should divide', () => {
+  expect(divide(6, 3)).toBe(2);
+  expect(divide(5, 2)).toBe(2.5);
+});
+
+test('should divide for zero', () => {
+  expect(divide(6, 0)).toBeNull();
+  expect(divide(5, 0)).toBeNull();
+});

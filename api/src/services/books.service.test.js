349f3c174e0a81cb16cb269f7bac5ff2e5cdbfe8
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-10-08 16:41:43 -0600

    added mocks to books service test

---
+++ api/src/services/books.service.test.js
@@ -0,0 +1,40 @@
+const BooksService = require('./books.service');
+
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter'
+  }
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {}
+};
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
+describe('Test for BooksService', () => {
+  let service;
+
+  beforeEach(() => {
+    service = new BooksService();
+    jest.clearAllMocks();
+  });
+
+  describe('test for getBooks', () => {
+    test('should return a list book', async() => {
+      // Arrange
+      // Act
+      const books = await service.getBooks({});
+      // Assert
+      expect(books.length).toEqual(1);
+    });
+    test('should return a list book', async() => {
+      // Arrange
+      // Act
+      const books = await service.getBooks({});
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter');
+    });
+  });
+});
